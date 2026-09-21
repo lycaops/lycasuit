38a65500d9bd2d14225ef2a007454ec6598273bc
Dilan Fernando <hs.lycamobile.it@gmail.com>
2026-09-21 19:36:58 +0200

    fix reset pass 1

--- components/home/home-sidebar.tsx
+++ components/home/home-sidebar.tsx
@@ -22,7 +22,7 @@ function HomeSidebar({
   admin?: boolean
 }) {
   return (
-    <aside className="hidden w-72 shrink-0 flex-col overflow-y-auto bg-[#21264e] px-6 py-6 text-white lg:flex lg:h-dvh lg:max-h-dvh lg:sticky lg:top-0">
+    <aside className="hidden w-72 shrink-0 flex-col overflow-hidden bg-[#21264e] px-6 py-6 text-white lg:flex lg:h-dvh lg:max-h-dvh lg:sticky lg:top-0">
       <div className="flex w-full flex-col items-start text-left shrink-0">
         <img src="/lops.svg" alt="LycaOps" className="block h-auto w-full max-w-[220px] object-contain object-left" />
       </div>
